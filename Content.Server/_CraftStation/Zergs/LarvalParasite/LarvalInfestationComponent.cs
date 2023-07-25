227552c3a3ccb989451a4be4ea81dc16633ed66b
DeffOS <68462654+DeffOS@users.noreply.github.com>
2023-07-25 14:09:08 +0300

    Компоненты зергов, пока как планировка

---
+++ Content.Server/_CraftStation/Zergs/LarvalParasite/LarvalInfestationComponent.cs
@@ -0,0 +1,10 @@
+using Robust.Shared.Containers;
+
+namespace Content.Server.CraftStation.Zergs.LarvalParasite
+{
+    [RegisterComponent]
+    public sealed class LarvalInfestationComponent : Component
+    {
+        public Container ImplantContainer = default!;
+    }
+}
